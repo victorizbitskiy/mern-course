503c33ed030fcf5ac9d7ddb54488ac3c26fe1160
victorizbitskiy <victor.izbitskiy@yandex.ru>
2021-11-01 23:36:25 +0300

    added work with localStorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { useRoutes } from './routes'
+import { useAuth } from './hooks/auth.hook'
+import { AuthContext } from './context/AuthContext'
 import 'materialize-css'
-import { useRoutes } from './routes';
 
 function App() {
-  const routes = useRoutes(false)
+  const { token, login, logout, userId } = useAuth()
+  const isAuthenticated = !!token // приведение к bool
+  const routes = useRoutes(isAuthenticated)
   return (
-    <Router>
-      <div className="container">
-        <h1>{routes}</h1>
-      </div>
-    </Router>
-  );
+    <AuthContext.Provider value={{
+      token, login, logout, userId, isAuthenticated
+    }}>
+      <Router>
+        <div className="container">
+          {routes}
+        </div>
+      </Router>
+    </AuthContext.Provider>
+  )
 }
 
 export default App
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useHttp } from '../hooks/http.hook'
 import { useMessage } from '../hooks/message.hook'
+import { AuthContext } from '../context/AuthContext'
 
 export const AuthPage = () => {
+  const auth = useContext(AuthContext)
   const message = useMessage()
   const { loading, request, error, clearError } = useHttp()
   const [form, setForm] = useState({
@@ -26,14 +28,14 @@ export const AuthPage = () => {
     try {
       const data = await request('/api/auth/register', 'POST', { ...form })
       message(data.message)
-    } catch (e) {}
+    } catch (e) { }
   }
 
   const loginHandler = async () => {
     try {
       const data = await request('/api/auth/login', 'POST', { ...form })
-      message(data.message)
-    } catch (e) {}
+      auth.login(data.token, data.userId)
+    } catch (e) { }
   }
 
   return (
